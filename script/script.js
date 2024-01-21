e7ec608f1754c8fa8cdaa24a794a7da53cbd44eb
Josh-Manley <joshmanley45@gmail.com>
2024-01-20 18:51:20 -0700

    added place holder questions in javascript

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const questions = [
+  {
+    question: "What is the capital of Utah?",
+    options: ["Salt Lake", "Las Vegas", "Berlin"],
+    correctAnswer: "Salt Lake"
+  },
+
+  {
+    question: "What is the name?",
+    options: ["name", "Bob"],
+    correctAnswer: "Bob"
+  }
+]
